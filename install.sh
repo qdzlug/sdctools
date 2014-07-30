22bbff4d47a64a0079f6465f425a1b661fecf1b8
Philip Borenstein <philip.borenstein@joyent.com>
2014-07-29 23:20:17 -0400

    Make links in /usr/local/bin

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# deletemachines images         machines       provision      state
+pushd $(dirname $0) > /dev/null
+THISDIR=$(pwd)
+popd > /dev/null
+
+ln -s $THISDIR/deletemachines /usr/local/bin/deletemachines
+ln -s $THISDIR/images /usr/local/bin/images
+ln -s $THISDIR/machines /usr/local/bin/machines
+ln -s $THISDIR/provision /usr/local/bin/provision
+ln -s $THISDIR/state /usr/local/bin/state
+
